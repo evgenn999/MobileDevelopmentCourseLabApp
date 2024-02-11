587393ee2a2206dcab48e8865bea2dacd7af3e05
evgenn999 <evgenn.99@yandex.ru>
2024-02-11 10:14:33 +0500

    task1

--- app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/presentation/view/article/ArticleFragment.kt
+++ app/src/main/kotlin/com/example/mobiledevelopmentcourselabapp/presentation/view/article/ArticleFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.example.mobiledevelopmentcourselabapp.R
 import com.example.mobiledevelopmentcourselabapp.databinding.FragmentArticleBinding
 
 class ArticleFragment : Fragment() {
@@ -22,6 +25,13 @@ class ArticleFragment : Fragment() {
         val root: View = binding.root
         // Обращайся к элементам View здесь
 
+        context?.let {
+            Glide
+                .with(it)
+                .load("https://studzachet.ru/d2.jpg")
+                .placeholder(AppCompatResources.getDrawable(it, R.drawable.baseline_school_24))
+                .into(binding.mainPhoto)
+        }
         return root
     }
 
